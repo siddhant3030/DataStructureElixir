283f4673fc9157a9dfb7a33788dbb12ef7ce11d0
siddhant3030 <ssiddhant3030@gmail.com>
2020-01-30 21:22:46 +0530

    comments added for execution

--- lib/bst.ex
+++ lib/bst.ex
@@ -1,16 +1,29 @@
 defmodule BST do
 
-  def new(element) do
-    %{element: element, left: :leaf, right: :leaf}
+  def new(value) do
+    %{value: value, left: :leaf, right: :leaf}
   end
 
-  def insert(:element, node_value), do: new node_value
+# iex(1)> tree = BST.new(100)
+# %{left: :leaf, right: :leaf, value: 100}
+# iex(2)> tree1 = BST.insert(tree, 200)
+# %{left: :leaf, right: %{left: :leaf, right: :leaf, value: 200}, value: 100}
+# iex(3)> tree2 = BST.insert(tree, 50)
+# %{left: %{left: :leaf, right: :leaf, value: 50}, right: :leaf, value: 100}
+# iex(4)> tree2 = BST.insert(tree1, 50)
+# %{
+#   left: %{left: :leaf, right: :leaf, value: 50},
+#   right: %{left: :leaf, right: :leaf, value: 200},
+#   value: 100
+# }
 
-  def insert(%{element: element, left: left, right: right}, node_value) do
-    if node_value < element do
-      %{element: element, left: insert(left, node_value), right: right}
+  def insert(:leaf, node_value), do: new node_value
+
+  def insert(%{value: value, left: left, right: right}, node_value) do
+    if node_value < value do
+      %{value: value, left: insert(left, node_value), right: right}
     else
-      %{element: element, left: left, right: insert(right, node_value)}
+      %{value: value, left: left, right: insert(right, node_value)}
     end
   end
 end
