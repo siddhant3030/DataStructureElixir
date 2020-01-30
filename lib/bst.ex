c2d4e290009c9abc36b240c62f678559d4f6a19b
siddhant3030 <ssiddhant3030@gmail.com>
2020-01-30 20:48:07 +0530

    insert logic added

--- lib/bst.ex
+++ lib/bst.ex
@@ -5,4 +5,12 @@ defmodule BST do
   end
 
   def insert(:element, node_value), do: new node_value
+
+  def insert(%{element: element, left: left, right: right}, node_value) do
+    if node_value < element do
+      %{element: element, left: insert(left, node_value), right: right}
+    else
+      %{element: element, left: left, right: insert(right, node_value)}
+    end
+  end
 end
