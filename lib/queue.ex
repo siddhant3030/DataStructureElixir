e7941ced2120284c25dcc8a0799b5ad07899e03c
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-01 19:15:25 +0530

    front added for the queue

--- lib/queue.ex
+++ lib/queue.ex
@@ -8,7 +8,21 @@ defmodule Queue do
   def put(%Queue{front: [], rear: rear = [_]}, item) do
     %Queue{front: rear, rear: [item]}
   end
+
   def put(%Queue{rear: rear} = queue, item) do
     %Queue{queue|rear: [item | rear]}
   end
+
+  def front(%Queue{front: front = [_], rear: []}, item) do
+    %Queue{front: [item], rear: front}
+  end
+
+#   a |> Queue.front(12) |> Queue.front(1222111) |> Queue.front(1212)
+# %Queue{front: [1212, 1222111], rear: '\f'}
+
+  def front(%Queue{front: front} = queue, item) do
+    %Queue{queue|front: [item | front]}
+  end
+
+
 end
