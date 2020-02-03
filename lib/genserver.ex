8276b7dde8781c6a271265b1205b77651e362120
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-03 08:42:45 +0530

    handle state function

--- lib/genserver.ex
+++ lib/genserver.ex
@@ -10,4 +10,13 @@ defmodule Basic do
     greetings = %{:greeting => initial_data}
     {:ok, greetings}
   end
+
+  def get_my_state(process_id) do
+    the_state = GenServer.call(process_id, {:get_the_state})
+    IO.inspect the_state
+  end
+
+  def handle_call({:get_the_state}, _from, my_state) do
+    {:reply, my_state, my_state}
+  end
 end
