51e3c106ea5f6840973334235e0052353be33dd4
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-03 09:22:42 +0530

    It's better

--- lib/books.ex
+++ lib/books.ex
@@ -2,10 +2,10 @@ defmodule Books do
   alias PackageReceiver
   def get_those_package_out do
     packages = ["adasd", "dsadasd", "sdadefe", "wqewqe", "eqwereq"]
-    {:ok, pid} = PackageReceiver.start_link
+
     Enum.each(packages, fn(package) ->
       IO.puts "delivering package: #{package}"
-
+      {:ok, pid} = PackageReceiver.start_link
       PackageReceiver.leave_at_the_door(pid, package)
     end)
 
