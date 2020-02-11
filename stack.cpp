71c578989d87d0a2fb09442dc9f33cc819c58841
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-11 17:37:23 +0530

    added functionality in c++ code

--- stack.cpp
+++ stack.cpp
@@ -9,6 +9,43 @@ class Stack{
     int arr[5];
 
     public: 
+        stack ()
+        {
+            top = -1
+            for(int i=0; i<5; i++){
+                arr[i] = 0;
+            }
+        }
+
+        bool isEmpty()
+        {
+            if(top==-1)
+                return true;
+            else
+                return false;
+        }
+
+        bool isFull()
+        {
+            if(top==4)
+                return true;
+            else
+                return false;
+        }
+
+        void push(int val)
+        {
+            if(isFull())
+            {
+                cout<<"stack overflow"<<endl;
+            }
+            else
+            {
+                top++;
+                arr[top] = val;
+            }
+
+        }
 };
 
 int main()
