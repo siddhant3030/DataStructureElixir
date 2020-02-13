ac0276c223b1e3a81b49ed98e5fb411b1bc32b10
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-13 20:58:45 +0530

    atom

--- lib/k.ex
+++ lib/k.ex
@@ -1,7 +1,11 @@
 defmodule BST.K do
   @spec main :: :ok
   def main do
-    name = IO.gets("what is your name?") |> String.trim
-    IO.puts "Hello #{name}"
+    data_stuff()
+  end
+
+  def data_stuff do
+    # my_int = 123
+    IO.puts "Atom is #{is_atom(:sid)}"
   end
 end
